d13ad2db5bf4a05a269004330432aff5cd72d56b
Phoenix-tear <ra8540@srmist.edu.in>
2024-03-08 17:03:49 +0530

    added event tracking

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
-const countToDate = new Date("2024-03-12T10:00:00");
 let previousTimeBetweenDates;
+let changingdiv = document.querySelector(".red-text");
+let staticdiv = document.querySelector(".white-text");
+let container = document.querySelector(".container");
+let events = ["Inaugration Ceremony", "Hackathon Begins", "Lunch Break", "Mentor Review 1", "Dinner break", "Mentor Review 2","Mentor Review 3","Shortlisting of teams", "Final Presentation","Result Announcement","Closing Ceremony"]
 setInterval(() => {
-    const currentDate = new Date();
-    const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
+    let currentDate = new Date();
+    if (currentDate < new Date("2024-03-11T10:00:00")) {
+        var countToDate = new Date("2024-03-11T10:00:00");
+        changingdiv.textContent = events[0];
+    }
+    else if(currentDate > new Date("2024-03-11T10:00:00") && (currentDate < new Date("2024-03-11T11:30:00"))) {
+        var countToDate = new Date("2024-03-11T11:30:00");
+        changingdiv.textContent = events[1]
+    }
+    else if(currentDate > new Date("2024-03-11T11:30:00") && (currentDate < new Date("2024-03-11T14:00:00"))) {
+        var countToDate = new Date("2024-03-11T14:00:00");
+        changingdiv.textContent = events[2]
+    }
+    else if(currentDate > new Date("2024-03-11T14:00:00") && (currentDate < new Date("2024-03-11T17:00:00"))) {
+        var countToDate = new Date("2024-03-11T17:00:00");
+        changingdiv.textContent = events[3]
+    }
+    else if(currentDate > new Date("2024-03-11T17:00:00") && (currentDate < new Date("2024-03-11T20:00:00"))) {
+        var countToDate = new Date("2024-03-11T20:00:00");
+        changingdiv.textContent = events[4]
+    }
+    else if(currentDate > new Date("2024-03-11T20:00:00") && (currentDate < new Date("2024-03-12T00:00:00"))) {
+        var countToDate = new Date("2024-03-12T00:00:00");
+        changingdiv.textContent = events[5]
+    }
+    else if(currentDate > new Date("2024-03-12T00:00:00") && (currentDate < new Date("2024-03-12T09:00:00"))) {
+        var countToDate = new Date("2024-03-12T09:00:00");
+        changingdiv.textContent = events[6]
+    }
+    else if(currentDate > new Date("2024-03-12T09:00:00") && (currentDate < new Date("2024-03-12T11:00:00"))) {
+        var countToDate = new Date("2024-03-12T11:00:00");
+        changingdiv.textContent = events[7]
+    }
+    else if(currentDate > new Date("2024-03-12T11:00:00") && (currentDate < new Date("2024-03-12T13:00:00"))) {
+        var countToDate = new Date("2024-03-12T13:00:00");
+        changingdiv.textContent = events[8]
+    }
+    else if(currentDate > new Date("2024-03-12T13:00:00") && (currentDate < new Date("2024-03-12T15:30:00"))) {
+        var countToDate = new Date("2024-03-12T15:30:00");
+        changingdiv.textContent = events[9]
+    }
+    else if(currentDate > new Date("2024-03-12T15:30:00") && (currentDate < new Date("2024-03-12T16:00:00"))) {
+        var countToDate = new Date("2024-03-12T16:00:00");
+        changingdiv.textContent = events[10]
+    }
+    else{
+        staticdiv.textContent = "THANKS FOR";
+        changingdiv.textContent = "PARTICIPATING IN HACKNOVA !!!";
+        container.style = "display:none";
+    }
+    
+        const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
 
     flipAllCards(timeBetweenDates);
 
